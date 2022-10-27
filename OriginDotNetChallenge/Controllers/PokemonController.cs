2ce2fa7c48da98471e5e3774ad1ea5770433656e
Victor Scherer <victorescherer@hotmail.com>
2022-10-27 12:11:06 -0300

    feat: adding get pokemons

--- OriginDotNetChallenge/Controllers/PokemonController.cs
+++ OriginDotNetChallenge/Controllers/PokemonController.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Headers;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OriginDotNetChallenge.Controllers;
@@ -6,9 +8,49 @@ namespace OriginDotNetChallenge.Controllers;
 [Route("pokemon")]
 public class PokemonController : ControllerBase
 {
-    public IActionResult Get()
+    public readonly HttpClient _client;
+
+    public PokemonController(HttpClient client)
     {
-        return Ok("hello world");
+        _client = client;
     }
     
+    public async Task<IActionResult> Get(int page = 1)
+    {
+        _client.BaseAddress = new Uri("https://pokeapi.co/api/");
+        _client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("origin-user", 
+                "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY2MjA0MjMzNCwiaWF0IjoxNjYyMDQyMzM0fQ.xi3uKpbHXXxE5iTOkDrkHJfpXQhGQGjLHXwC1SE-kFI");
+        var response = await _client.GetAsync(BuildGetUrl(page));
+
+        var result = await response.Content.ReadAsStringAsync();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+        var deserializedObject = JsonSerializer.Deserialize<PokeApiResponse>(result, options);
+        
+
+        return Ok(deserializedObject?.Results);
+    }
+
+    public string BuildGetUrl(int page)
+    {
+        var limit = 20;
+        var offset = page - 1;
+        
+        return $"v2/pokemon?offset={offset}&limit={limit}";
+    }
+    
+}
+
+public class PokeApiResponse
+{
+    public List<PokemonDTO> Results { get; set; }
+}
+
+public class PokemonDTO
+{
+    public string Name { get; set; }
+    public string Url { get; set; }
 }
